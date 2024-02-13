8421e3544bfc0ece8abfe137af199928502c28c2
HidekiS <20212604@aloe.ulima.edu.pe>
2024-02-13 02:22:33 -0500

    login endpoint post

--- proyectos/urls.py
+++ proyectos/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import verEquiposEndpoint, verEquiposPathParameterEndpoint, loginEndpoint
+from .views import verEquiposEndpoint, verEquiposPathParameterEndpoint, loginEndpoint, loginPostEndpoint
 
 urlpatterns = [
     path("ver-equipos", verEquiposEndpoint),
     path("ver-equipos-path/<str:filtro>", verEquiposPathParameterEndpoint),
-    path("login/<str:username>/<str:password>", loginEndpoint)
+    path("login/<str:username>/<str:password>", loginEndpoint),
+    path("login", loginPostEndpoint)
 ]
